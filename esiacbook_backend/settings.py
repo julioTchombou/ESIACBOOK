18ad191ad6b0e168ba5390e504f0e6afb3da82d3
julio tchombou <jtchombou@gmail.com>
2026-08-31 16:49:48 +0100

    Configuration envoi email SMTP en production

--- esiacbook_backend/settings.py
+++ esiacbook_backend/settings.py
@@ -40,7 +40,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Application definition
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
